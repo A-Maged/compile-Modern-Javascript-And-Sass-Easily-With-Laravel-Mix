d376aea60308c8af88dabfb2c51c35f452344880
A-Maged <26769761+A-Maged@users.noreply.github.com>
2020-05-06 08:55:51 +0200

    added browserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,7 @@ const mix = require('laravel-mix');
 
 /*
  * Extend laravel mix to add more functionalities
- * Must come after "mix" is defined 
+ * Must come after "mix" is defined
  */
 require('mix-tailwindcss');
 require('laravel-mix-purgecss');
@@ -12,7 +12,6 @@ require('laravel-mix-purgecss');
 glob.sync('src/style/blocks/**/*.scss').forEach((file_path) => {
     mix.sass(file_path, 'build/blocks/');
 });
-
 
 mix.js('src/js/index.js', 'build')
     .sass('src/style/style.scss', 'build')
@@ -25,5 +24,18 @@ mix.js('src/js/index.js', 'build')
             '**/*.twig',
         ] /* Remove unused CSS in files with these extensions */,
     })
-    .sourceMaps(!mix.inProduction(), 'source-map') /* Only add source maps if not building for production */
+    .sourceMaps(
+        !mix.inProduction(),
+        'source-map'
+    ) /* Only add source maps if not building for production */
+    .browserSync({
+        proxy: process.env.URL,
+        files: [
+            './**/*.twig',
+            './**/*.php',
+            './**/*.js',
+            './**/*.scss',
+        ] /* Reload if any file with one of these extensions is changed */,
+        ignore: ['./.git', './**/node_modules', './build', './vendor'],
+    })
     .disableSuccessNotifications(); /* Don't alert when build is successful (because it gets annoying over time) */
